9380abdb6ef985268ea4f288698554ab384497db
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-07 15:50:24 +0900

    springboot 세팅

---
+++ src/main/java/com/ss/heartlinkapi/HeartlinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.ss.heartlinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartlinkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartlinkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss/heartlinkapi/HeartlinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.heartlinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartlinkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
