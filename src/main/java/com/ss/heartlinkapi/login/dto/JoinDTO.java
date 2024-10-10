f2b673c29534422b52bba7f220ce1461ac8db1e2
dduddo0226 <gusbk0112@gmail.com>
2024-10-10 09:33:57 +0900

    feat: JoinDTO 생성

--- src/main/java/com/ss/heartlinkapi/login/dto/JoinDTO.java
+++ src/main/java/com/ss/heartlinkapi/login/dto/JoinDTO.java
@@ -1,8 +1,18 @@
 package com.ss.heartlinkapi.login.dto;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class JoinDTO {
-
+	
+	private String loginId;
+	private String name;
+	private String password;
+	private String email;
+	private char gender;
+	private String nickname;
+	private LocalDate birthDate;
+		
 }
