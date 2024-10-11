c6009e7e3b0f7eae95b7c4445151f89c4da0f1a8
dduddo0226 <gusbk0112@gmail.com>
2024-10-11 15:41:10 +0900

    feat: LoginDTO생성

---
+++ src/main/java/com/ss/heartlinkapi/login/dto/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.ss.heartlinkapi.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+	
+	private String loginId;
+	private String password;
+	
+}
