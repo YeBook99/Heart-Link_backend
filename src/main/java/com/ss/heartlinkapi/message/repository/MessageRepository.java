8e89cd958e9241be2bb9efd6b8b1700aa2d08f34
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-14 12:23:43 +0900

    chore: MessageRepository 수정

--- src/main/java/com/ss/heartlinkapi/message/repository/MessageRepository.java
+++ src/main/java/com/ss/heartlinkapi/message/repository/MessageRepository.java
@@ -1,7 +1,8 @@
 package com.ss.heartlinkapi.message.repository;
 
+import com.ss.heartlinkapi.message.dto.MessageEntity;
 import com.ss.heartlinkapi.message.dto.MessageRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageRepository extends JpaRepository<MessageRoomEntity, Long> {
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
 }
