c1b555b2ad65e398e693ffe8f47cc0b08916685a
YeBook99 <pop0131@naver.com>
2024-10-14 12:05:16 +0900

    feat create linktag entity

--- src/main/java/com/ss/heartlinkapi/linkmatch/repository/CoupleMatchAnswerRepository.java
+++ src/main/java/com/ss/heartlinkapi/linkmatch/repository/CoupleMatchAnswerRepository.java
@@ -11,15 +11,9 @@ import java.util.List;
 public interface CoupleMatchAnswerRepository extends JpaRepository<LinkMatchAnswerEntity, Long> {
 
     // 매칭 성공 실패 여부 확인(오늘 날짜 기준 매칭 성공 시 1, 실패 시 0, 한 사용자만 등록했을 경우 2 반환)
-    @Query("SELECT " +
-            "CASE " +
-            "   WHEN COUNT(DISTINCT a.choice) = 1 AND COUNT(a.choice) > 1 THEN 1 " + // 두 사용자가 동일한 선택을 했을 경우
-            "   WHEN COUNT(DISTINCT a.choice) > 1 THEN 0 " + // 서로 다른 선택을 했을 경우
-            "   ELSE 2 " + // 선택이 없는 경우 또는 단일 선택만 있을 경우
-            "END " +
-            "FROM LinkMatchAnswerEntity a " +
-            "WHERE a.coupleId.coupleId = :coupleId " +
-            "AND DATE(a.createdAt) = CURRENT_DATE")
+    @Query("SELECT CASE WHEN COUNT(DISTINCT a.choice) = 1 AND COUNT(a.choice) > 1 THEN 1 " + // 두 사용자가 동일한 선택을 했을 경우
+            "WHEN COUNT(DISTINCT a.choice) > 1 THEN 0 ELSE 2 END FROM LinkMatchAnswerEntity a " +
+            "WHERE a.coupleId.coupleId = :coupleId AND DATE(a.createdAt) = CURRENT_DATE")
     int checkTodayMatching(@Param("coupleId") Long coupleId);
 
     // 매치 답변 내역 조회
