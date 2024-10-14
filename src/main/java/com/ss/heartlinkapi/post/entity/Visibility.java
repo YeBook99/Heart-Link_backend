26e79ed51df5d78b3106210a098ff73c76f035af
iwecs <bjsoon3333@gmail.com>
2024-10-14 16:16:25 +0900

    게시글, 게시글 첨부파일 엔티티 작성

---
+++ src/main/java/com/ss/heartlinkapi/post/entity/Visibility.java
@@ -0,0 +1,8 @@
+package com.ss.heartlinkapi.post.entity;
+
+public enum Visibility {
+	PUBLIC,		// 전체 공개
+	PRIVATE,	// 비공개
+	FOLLWERS	// 팔로워만 공개
+
+}
---
+++ src/main/java/com/ss/heartlinkapi/user/entity/PostFileEntity.java
@@ -0,0 +1,39 @@
+package com.ss.heartlinkapi.user.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.ss.heartlinkapi.post.entity.PostEntity;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "post_file")
+public class PostFileEntity {
+	@Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long postFileId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", nullable = false)
+	private PostEntity postId;
+	
+	@Column(name = "file_url", nullable = false)
+	private String fileUrl;
+	
+	@Column(name = "file_type", length = 50)
+	private String file_type;
+	
+	@Column(name = "sort_order")
+	private int sortOrder;
+
+}
