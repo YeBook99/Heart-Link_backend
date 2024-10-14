6d1b68224b055bee42f307c066a0b26277fbeb2a
dduddo0226 <gusbk0112@gmail.com>
2024-10-14 12:23:55 +0900

    chore: SecurityConfig 파일 위치 변경
    로그인 컨피그 패키지에 있던 시큐리티컨피그를 일반 컨피그 설정 패키지로 위치 변경

--- src/main/java/com/ss/heartlinkapi/config/SecurityConfig.java
+++ src/main/java/com/ss/heartlinkapi/config/SecurityConfig.java
@@ -1,4 +1,6 @@
-package com.ss.heartlinkapi.login.config;
+package com.ss.heartlinkapi.config;
+
+import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.ss.heartlinkapi.login.jwt.JWTFilter;
+import com.ss.heartlinkapi.login.jwt.JWTUtil;
 import com.ss.heartlinkapi.login.jwt.LoginFilter;
 import com.ss.heartlinkapi.login.service.CustomUserDetailsService;
+import com.ss.heartlinkapi.login.service.JWTService;
+import com.ss.heartlinkapi.login.service.RefreshTokenService;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +29,20 @@ public class SecurityConfig {
 	
 	private final AuthenticationConfiguration authenticationConfiguration;
 	private final CustomUserDetailsService customUserDetailsService;
-	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration,CustomUserDetailsService customUserDetailsService) {
+	private final JWTUtil jwtUtil;
+	private final JWTService jwtService;
+	private final RefreshTokenService refreshTokenService;
+
+	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration,
+			CustomUserDetailsService customUserDetailsService, JWTUtil jwtUtil, JWTService jwtService,
+			RefreshTokenService refreshTokenService) {
 		this.authenticationConfiguration = authenticationConfiguration;
 		this.customUserDetailsService = customUserDetailsService;
+		this.jwtUtil = jwtUtil;
+		this.jwtService = jwtService;
+		this.refreshTokenService = refreshTokenService;
 	}
-	
+
 	@Bean
 	public AuthenticationManager authenticationManager() throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
@@ -36,13 +54,18 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .formLogin(login -> login.disable())
                 .httpBasic(basic -> basic.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.antMatchers("/user/join").permitAll()
                         .antMatchers("/user/idcheck").permitAll()
+                        .antMatchers("/reissue").permitAll()
+                        //토큰 role값 검증 확인용
+                        //.antMatchers("/user/check").hasRole("USER")
                         // 예능 전용
                         .antMatchers("/couple/**", "/admin/**").permitAll()
                         .anyRequest().authenticated());
-        http.addFilterAt(new LoginFilter(customUserDetailsService, bCryptPasswordEncoder(), authenticationManager()), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JWTFilter(jwtService), LoginFilter.class);
+        http.addFilterAt(new LoginFilter(customUserDetailsService, bCryptPasswordEncoder(), authenticationManager(),jwtUtil,refreshTokenService), UsernamePasswordAuthenticationFilter.class);
         
 			return http.build();	
 	}
@@ -52,6 +75,22 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration configuration = new CorsConfiguration();
+		
+		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+		configuration.setAllowedMethods(Collections.singletonList("*"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Collections.singletonList("*"));
+		configuration.setMaxAge(3600L);
+		configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    
+	    return source;
+	}
 	
 }
