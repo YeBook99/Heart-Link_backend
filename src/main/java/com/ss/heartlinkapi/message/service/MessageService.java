7eb300c87b6b5d94d76c07d1d0264f8d0a6ad747
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-21 10:44:26 +0900

    feat: message 저장기능 service 작성

--- src/main/java/com/ss/heartlinkapi/message/service/MessageService.java
+++ src/main/java/com/ss/heartlinkapi/message/service/MessageService.java
@@ -4,6 +4,7 @@ import com.ss.heartlinkapi.message.dto.ChatMsgListDTO;
 import com.ss.heartlinkapi.message.entity.MessageEntity;
 import com.ss.heartlinkapi.message.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,15 +13,19 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MessageService {
 
     private final MessageRepository messageRepository;
 
+    /*
+    * 사용자들의 대화내역은 채팅방 번호 즉 msgRoomId를 기본키로 해서 저장되므로
+    * 이를 기준으로 메세지를 list형태에 저장하여 가져온다.
+    */
     public List<ChatMsgListDTO> getAllChatMessage(Long msgRoomId) {
 
         List<ChatMsgListDTO> list = new ArrayList<>();
-
-//
+        
         List<MessageEntity> messageEntities = messageRepository.findByMsgRoomId(msgRoomId);
 
         for (MessageEntity entity : messageEntities) {
@@ -33,6 +38,7 @@ public class MessageService {
             boolean isRead = entity.isRead();
 
             ChatMsgListDTO chatMsgListDTO = ChatMsgListDTO.builder()
+                    .msgRoomId(msgRoomId)
                     .senderId(senderId)
                     .content(content)
                     .emoji(emoji)
@@ -45,5 +51,24 @@ public class MessageService {
         }
         return list;
     }
+
+//    사용자가 보낸 채팅을 저장
+    public void saveChatMessage(ChatMsgListDTO chatMsgListDTO) {
+
+        MessageEntity messageEntity = new MessageEntity();
+        
+        messageEntity.setMsgRoomId(chatMsgListDTO.getMsgRoomId());
+        messageEntity.setSenderId(chatMsgListDTO.getSenderId());
+        messageEntity.setContent(chatMsgListDTO.getContent());
+        messageEntity.setEmoji(chatMsgListDTO.getEmoji());
+        messageEntity.setImgUrl(chatMsgListDTO.getImageUrl());
+        messageEntity.setRead(chatMsgListDTO.isRead());
+        messageEntity.setCreatedAt(chatMsgListDTO.getLastMessageTime());
+
+        messageRepository.save(messageEntity);
+    }
+
+
+
 }
 
