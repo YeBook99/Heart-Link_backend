328ffdc4c796d85aefabaa0ecb967a1f27733ba3
iwecs <bjsoon3333@gmail.com>
2024-10-21 10:32:40 +0900

    좋아요 엔티티 작성

---
+++ src/main/java/com/ss/heartlinkapi/like/entity/LikeEntitiy.java
@@ -0,0 +1,53 @@
+package com.ss.heartlinkapi.like.entity;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.ss.heartlinkapi.comment.entity.CommentEntity;
+import com.ss.heartlinkapi.post.entity.PostEntity;
+import com.ss.heartlinkapi.post.entity.Visibility;
+import com.ss.heartlinkapi.user.entity.UserEntity;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "like")
+@EntityListeners(AuditingEntityListener.class)
+public class LikeEntitiy {
+	@Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long likeId;					// 좋아요 아이디
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserEntity userId;				// 회원 아이디
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", nullable = false)
+	private PostEntity postId;				// 게시글 아이디
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "comment_id", nullable = false)
+	private CommentEntity commentId;		// 댓글 아이디
+	
+	@CreatedDate
+	@Column(name = "created_at", nullable= false, updatable = false)
+	private Timestamp createdAt;			// 생성일시
+
+}
