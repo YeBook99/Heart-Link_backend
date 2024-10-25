632b8ce40ad2ff61bb8cf7cee184e12f84d96c86
dduddo0226 <gusbk0112@gmail.com>
2024-10-25 18:43:51 +0900

    faet:OAuth2Controller 수정
    로그인 아이디 저장 컨트롤러 생성

--- src/main/java/com/ss/heartlinkapi/oauth2/controller/OAuth2Controller.java
+++ src/main/java/com/ss/heartlinkapi/oauth2/controller/OAuth2Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ss.heartlinkapi.oauth2.service.LoginIdService;
 import com.ss.heartlinkapi.oauth2.service.PhoneService;
 
 
@@ -17,16 +18,24 @@ import com.ss.heartlinkapi.oauth2.service.PhoneService;
 public class OAuth2Controller {
 	
 	private final PhoneService phoneService;
+	private final LoginIdService loginIdService;
 	
-	public OAuth2Controller(PhoneService phoneService) {
+	public OAuth2Controller(PhoneService phoneService, LoginIdService loginIdService) {
 		this.phoneService = phoneService;
+		this.loginIdService = loginIdService;
 	}
-
 
 	@PostMapping("/phone")
 	public ResponseEntity<Void> setPhoneNumber(@RequestBody Map<String, String> request,@RequestParam String providerId){
 		String phone = request.get("phone");
 		phoneService.storeTempPhone(providerId, phone);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PostMapping("/loginId")
+	public ResponseEntity<Void> setLoginId(@RequestBody Map<String, String> request,@RequestParam String providerId){
+		String loginId = request.get("loginId");
+		loginIdService.storeTempLoginId(providerId, loginId);
 		return ResponseEntity.ok().build();
 	}
 	
