f6b23d48d720b82ed1ce057eaf8b257c4be3e614
dduddo0226 <gusbk0112@gmail.com>
2024-10-25 18:41:06 +0900

    feat: SocialRepository 추가
    프로바이더 아이디로 유저 가져오는 메서드 추가

--- src/main/java/com/ss/heartlinkapi/user/repository/SocialRepository.java
+++ src/main/java/com/ss/heartlinkapi/user/repository/SocialRepository.java
@@ -1,9 +1,16 @@
 package com.ss.heartlinkapi.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ss.heartlinkapi.user.entity.SocialEntity;
+import com.ss.heartlinkapi.user.entity.UserEntity;
 
-public interface SocialRepository extends JpaRepository<SocialEntity, Long>{
+public interface SocialRepository extends JpaRepository<SocialEntity, Long> {
 	boolean existsByProviderId(String providerId);
+
+	@Query("SELECT s.userEntity FROM SocialEntity s WHERE s.providerId = :providerId")
+	UserEntity findUserByProviderId(@Param("providerId") String providerId);
+
 }
