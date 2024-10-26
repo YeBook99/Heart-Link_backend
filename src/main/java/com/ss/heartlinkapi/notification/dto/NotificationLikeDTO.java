85c612303c52d1c37b4b0cef2dc963d244f008ae
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-27 00:52:07 +0900

    feat: NotificationLikeDTO 작성

---
+++ src/main/java/com/ss/heartlinkapi/notification/dto/NotificationLikeDTO.java
@@ -0,0 +1,15 @@
+package com.ss.heartlinkapi.notification.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotificationLikeDTO {
+    private String url;
+    private String postImgUrl;
+    private String message;
+}
