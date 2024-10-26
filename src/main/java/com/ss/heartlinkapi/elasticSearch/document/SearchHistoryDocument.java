74779abb74edc1ece1cf6716311a9980069459cf
YeBook99 <pop0131@naver.com>
2024-10-26 20:35:35 +0900

    feat 엘라스틱 검색기록 인덱스 생성

--- src/main/java/com/ss/heartlinkapi/elasticSearch/document/SearchHistoryDocument.java
+++ src/main/java/com/ss/heartlinkapi/elasticSearch/document/SearchHistoryDocument.java
@@ -1,17 +1,13 @@
-package com.ss.heartlinkapi.search.document;
+package com.ss.heartlinkapi.elasticSearch.document;
 
-import com.ss.heartlinkapi.user.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Document(indexName = "search_history")
@@ -28,10 +24,6 @@ public class SearchHistoryDocument {
     private String keyword;
     @Field(type = FieldType.Keyword)
     private String type;
-    @CreatedDate
     @Field(type = FieldType.Date)
-    private LocalDateTime createdAt;
-    @LastModifiedDate
-    @Field(type = FieldType.Date)
-    private LocalDateTime updatedAt;
+    private LocalDateTime date;
 }
