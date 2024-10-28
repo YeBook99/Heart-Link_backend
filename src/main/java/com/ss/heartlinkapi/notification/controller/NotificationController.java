cfd12cb3dc9f6cbe98d851a57fa72ca3ed53b795
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-28 16:41:16 +0900

    feat: NotificationController getNotifications 메서드 작성

--- src/main/java/com/ss/heartlinkapi/notification/controller/NotificationController.java
+++ src/main/java/com/ss/heartlinkapi/notification/controller/NotificationController.java
@@ -1,10 +1,17 @@
 package com.ss.heartlinkapi.notification.controller;
 
+import com.ss.heartlinkapi.login.dto.CustomUserDetails;
+import com.ss.heartlinkapi.notification.dto.NotificationDTO;
 import com.ss.heartlinkapi.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,4 +27,11 @@ public class NotificationController {
         return notificationService.subscribe(userId);
     }
 
+
+//    알림 목록을 불러오는 기능
+    @GetMapping
+    public ResponseEntity<List<NotificationDTO>> getNotifications(@AuthenticationPrincipal CustomUserDetails user) {
+        List<NotificationDTO> notifications = notificationService.getNotifications(user);
+        return ResponseEntity.ok(notifications);
+    }
 }
