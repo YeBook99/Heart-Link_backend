68fcee195785271cd9f748cb107d93a428c33387
dduddo0226 <gusbk0112@gmail.com>
2024-10-28 17:07:55 +0900

    feat: BlockRepository 수정
    차단한 유저와 차단된 커플이 일치하는 차단이 존재하는지 확인

--- src/main/java/com/ss/heartlinkapi/block/repository/BlockRepository.java
+++ src/main/java/com/ss/heartlinkapi/block/repository/BlockRepository.java
@@ -4,8 +4,13 @@ import com.ss.heartlinkapi.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ss.heartlinkapi.block.entity.BlockEntity;
+import com.ss.heartlinkapi.couple.entity.CoupleEntity;
 
 public interface BlockRepository extends JpaRepository<BlockEntity, Long>{
 
     BlockEntity findByBlockedIdAndBlockerId(UserEntity blockedId, UserEntity blockerId);
+    
+    // 차단한 유저와 차단된 커플이 일치하는 차단이 존재하는지 확인
+    boolean existsByBlockerIdAndCoupleId(UserEntity blocker, CoupleEntity couple);
+
 }
