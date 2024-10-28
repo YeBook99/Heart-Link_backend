635d50fb398afd47670c9b3f009b2dee532dd6b6
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-28 16:40:31 +0900

    feat: NotificationDTO 작성

---
+++ src/main/java/com/ss/heartlinkapi/notification/dto/NotificationDTO.java
@@ -0,0 +1,17 @@
+package com.ss.heartlinkapi.notification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotificationDTO {
+    private String message;
+    private LocalDateTime createdAt;
+}
