86ad0193ef980556cc90d195c3ad01a3c239d573
dduddo0226 <gusbk0112@gmail.com>
2024-10-29 16:53:26 +0900

    feat: ProfileController 기능추가
    내 프로필에서 비밀번호 변경 기능 구현

--- src/main/java/com/ss/heartlinkapi/profile/controller/ProfileController.java
+++ src/main/java/com/ss/heartlinkapi/profile/controller/ProfileController.java
@@ -2,6 +2,7 @@ package com.ss.heartlinkapi.profile.controller;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -10,16 +11,17 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.ss.heartlinkapi.block.service.BlockService;
-import com.ss.heartlinkapi.couple.entity.CoupleEntity;
 import com.ss.heartlinkapi.couple.service.CoupleService;
 import com.ss.heartlinkapi.login.dto.CustomUserDetails;
+import com.ss.heartlinkapi.login.service.LoginService;
 import com.ss.heartlinkapi.profile.dto.ProfileDTO;
+import com.ss.heartlinkapi.profile.dto.UpdatePasswordDTO;
 import com.ss.heartlinkapi.profile.service.ProfileService;
 import com.ss.heartlinkapi.user.entity.ProfileEntity;
 import com.ss.heartlinkapi.user.entity.UserEntity;
@@ -30,14 +32,15 @@ public class ProfileController {
 
 	private final ProfileService profileService;
 	private final CoupleService coupleService;
-	private final BlockService blockService;
+	private final LoginService loginService;
 
-	public ProfileController(ProfileService profileService, CoupleService coupleService, BlockService blockService) {
+	public ProfileController(ProfileService profileService, CoupleService coupleService, LoginService loginService) {
 		this.profileService = profileService;
 		this.coupleService = coupleService;
-		this.blockService = blockService;
+		this.loginService = loginService;
 	}
 
+	/***************** 프로필 조회 ******************/
 	@GetMapping("/{userId}")
 	public ResponseEntity<?> selectProfile(@PathVariable Long userId,
 			@AuthenticationPrincipal CustomUserDetails loginUser) {
@@ -45,16 +48,6 @@ public class ProfileController {
 		UserEntity userEntity = profileService.findByUserId(userId);
 		if (userEntity == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저를 찾을 수 없습니다.");
-		}
-
-		CoupleEntity loginCouple = coupleService.findCoupleEntity(loginUser.getUserEntity());
-		if (blockService.isUserBlockedByCouple(userEntity, loginCouple)) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("날 차단한 유저의 프로필");
-		}
-
-		CoupleEntity coupleUser = coupleService.findCoupleEntity(userEntity);
-		if (blockService.isUserBlockedByCouple(loginUser.getUserEntity(), coupleUser)) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("내가 차단한 유저의 프로필");
 		}
 
 		ProfileEntity userProfile = profileService.selectProfile(userEntity);
@@ -75,6 +68,93 @@ public class ProfileController {
 		return ResponseEntity.ok(profileDTO);
 	}
 
+	/***************** 내 프로필에서 비밀번호 변경 ******************/
+
+	@PostMapping("/{userId}/update/password")
+	public ResponseEntity<?> updatePassword(@PathVariable Long userId, @RequestBody UpdatePasswordDTO updatePasswordDTO,
+			@AuthenticationPrincipal CustomUserDetails loginUser) {
+
+		String beforePassword = updatePasswordDTO.getBeforePassword();
+		String afterPassword = updatePasswordDTO.getAfterPassword();
+
+		UserEntity userEntity = profileService.findByUserId(userId);
+		if (userEntity == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저를 찾을 수 없습니다.");
+		}
+
+		if (!userId.equals(loginUser.getUserId())) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("수정 권한이 없습니다.");
+		}
+
+		boolean ismatch = loginService.checkPassword(userEntity, beforePassword);
+		if (!ismatch) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("이전 비밀번호가 일치하지 않습니다.");
+		}
+
+		boolean isUpdated = loginService.updatePassword(userEntity, afterPassword);
+		if (isUpdated) {
+			return ResponseEntity.ok("비밀번호가 성공적으로 변경되었습니다.");
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("비밀번호 변경 실패");
+		}
+
+	}
+
+	/***************** 애칭 수정 ******************/
+	@PostMapping("/{userId}/update/nickname")
+	public ResponseEntity<?> updateNickname(@PathVariable Long userId, @RequestBody Map<String, String> request,
+			@AuthenticationPrincipal CustomUserDetails loginUser) {
+
+		String nickName = request.get("nickName");
+
+		UserEntity userEntity = profileService.findByUserId(userId);
+		if (userEntity == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저를 찾을 수 없습니다.");
+		}
+
+		if (!userId.equals(loginUser.getUserId())) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("수정 권한이 없습니다.");
+		}
+
+		ProfileEntity profileEntity = profileService.findByUserEntity(userEntity);
+		if (profileEntity == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("프로필이 존재하지 않습니다.");
+		}
+
+		profileEntity.setNickname(nickName);
+		profileService.save(profileEntity);
+
+		return ResponseEntity.ok("애칭이 성공적으로 변경되었습니다.");
+
+	}
+
+	/***************** 상태메시지 수정 ******************/
+	@PostMapping("/{userId}/update/bio")
+	public ResponseEntity<?> updateProfileBio(@PathVariable Long userId, @RequestBody Map<String, String> request,
+			@AuthenticationPrincipal CustomUserDetails loginUser) {
+
+		String bio = request.get("bio");
+
+		UserEntity userEntity = profileService.findByUserId(userId);
+		if (userEntity == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저를 찾을 수 없습니다.");
+		}
+
+		if (!userId.equals(loginUser.getUserId())) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("수정 권한이 없습니다.");
+		}
+
+		ProfileEntity profileEntity = profileService.findByUserEntity(userEntity);
+		if (profileEntity == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("프로필이 존재하지 않습니다.");
+		}
+		profileEntity.setBio(bio);
+		profileService.save(profileEntity);
+
+		return ResponseEntity.ok("상태메시지가 수정되었습니다.");
+	}
+
+	/***************** 프로필 이미지 수정 ******************/
 	@PostMapping("/{userId}/update/img")
 	public ResponseEntity<?> updateProfileImage(@PathVariable Long userId, @RequestParam("img") MultipartFile img,
 			@AuthenticationPrincipal CustomUserDetails loginUser) {
@@ -102,20 +182,23 @@ public class ProfileController {
 			if (!fileExtension.matches("(?i)\\.(jpg|jpeg|png)$")) {
 				return ResponseEntity.badRequest().body("지원하지 않는 파일 형식입니다.");
 			}
-			
+
 			// UUID로 파일 이름 변경
 			String newFileName = UUID.randomUUID().toString() + fileExtension;
 			String filePath = currentPath + "/src/main/resources/static/img/" + newFileName;
 			// 파일 생성
 			img.transferTo(new File(filePath));
 			String imageUrl = "http://localhost:9090/img/" + newFileName;
-			
+
 			ProfileEntity profileEntity = profileService.findByUserEntity(userEntity);
-			if(profileEntity!=null) {
-				profileEntity.setProfile_img(imageUrl);
-				profileService.save(profileEntity);
+
+			if (profileEntity == null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("프로필이 존재하지 않습니다.");
 			}
-			
+
+			profileEntity.setProfile_img(imageUrl);
+			profileService.save(profileEntity);
+
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이미지 저장 오류 : " + e);
 		}
