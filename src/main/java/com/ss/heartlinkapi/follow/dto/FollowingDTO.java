1d7c6ffc309f14b8f5fbf470c2425e7a883b6f53
dduddo0226 <gusbk0112@gmail.com>
2024-10-29 19:09:11 +0900

    feat: FollowingDTO 생성

---
+++ src/main/java/com/ss/heartlinkapi/follow/dto/FollowingDTO.java
@@ -0,0 +1,12 @@
+package com.ss.heartlinkapi.follow.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FollowingDTO {
+	private Long followingUserId;
+	private String followingLoginId;
+	private String followingImg;
+}
