60c528f217f1c869b56f1e52d3ec4c5613666011
dduddo0226 <gusbk0112@gmail.com>
2024-10-29 19:10:06 +0900

    feat: FollowService 수정
    팔로워, 팔로잉 리스트 가져오는 기능 추가

--- src/main/java/com/ss/heartlinkapi/follow/controller/FollowingController.java
+++ src/main/java/com/ss/heartlinkapi/follow/controller/FollowingController.java
@@ -2,17 +2,22 @@ package com.ss.heartlinkapi.follow.controller;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ss.heartlinkapi.follow.dto.FollowDTO;
+import com.ss.heartlinkapi.follow.dto.FollowerDTO;
+import com.ss.heartlinkapi.follow.dto.FollowingDTO;
 import com.ss.heartlinkapi.follow.service.FollowService;
-import com.ss.heartlinkapi.user.entity.UserEntity;
+
 
 @RestController
-@RequestMapping("/following")
+@RequestMapping("/follow")
 public class FollowingController {
 
 	private final FollowService followService;
@@ -21,10 +26,31 @@ public class FollowingController {
 		this.followService = followService;
 	}
 
-	// 로그인한 회원의 팔로잉 찾기
-	@GetMapping("/{userId}")
-	public List<FollowDTO> getFollowing(@PathVariable Long userId) {
-		return followService.getFollowingIdsByUserId(userId);
+	/********** 회원이 팔로우한 유저 목록 보기 **********/
+	@GetMapping("/following/{userId}")
+	public ResponseEntity<?> getFollowing(@PathVariable Long userId) {
+	    try {
+	        List<FollowingDTO> followingList = followService.getFollowingByUserId(userId);
+	        return ResponseEntity.ok(followingList);
+	    } catch (EntityNotFoundException e) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저가 존재하지 않습니다.");
+	    } catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 에러 : "+e);
+	    }
 	}
-
+	
+	/********** 회원을 팔로우한 유저 목록 보기 **********/
+	@GetMapping("/follower/{userId}")
+	public ResponseEntity<?> getFollower(@PathVariable Long userId) {
+	    try {
+	        List<FollowerDTO> followerList = followService.getFollowersByUserId(userId);
+	        return ResponseEntity.ok(followerList);
+	    } catch (EntityNotFoundException e) {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("유저가 존재하지 않습니다.");
+	    } catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 에러 : " + e);
+	    }
+	}
+	
+	
 }
--- src/main/java/com/ss/heartlinkapi/follow/service/FollowService.java
+++ src/main/java/com/ss/heartlinkapi/follow/service/FollowService.java
@@ -5,26 +5,59 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import com.ss.heartlinkapi.follow.dto.FollowDTO;
+import com.ss.heartlinkapi.follow.dto.FollowerDTO;
+import com.ss.heartlinkapi.follow.dto.FollowingDTO;
 import com.ss.heartlinkapi.follow.entity.FollowEntity;
 import com.ss.heartlinkapi.follow.repository.FollowRepository;
+import com.ss.heartlinkapi.user.entity.ProfileEntity;
 import com.ss.heartlinkapi.user.entity.UserEntity;
+import com.ss.heartlinkapi.user.repository.ProfileRepository;
 
 @Service
 public class FollowService {
 
 	private final FollowRepository followRepository;
+	private final ProfileRepository profileRepository;
 
-	public FollowService(FollowRepository followRepository) {
+	public FollowService(FollowRepository followRepository, ProfileRepository profileRepository) {
 		this.followRepository = followRepository;
+		this.profileRepository = profileRepository;
 	}
 
-	// 로그인한 회원의 팔로잉 회원 정보
-	public List<FollowDTO> getFollowingIdsByUserId(Long userId) {
-		List<FollowEntity> follows = followRepository.findFollowingIdsByFollowerId(userId);
-
-		return follows.stream().map(follow -> new FollowDTO(follow.getFollowId(), follow.getFollowing().getLoginId()))
-				.collect(Collectors.toList());
+	/********** 유저가 팔로우하고 있는 타 유저 목록 **********/
+	public List<FollowingDTO> getFollowingByUserId(Long userId) {
+		List<FollowEntity> followings = followRepository.findByFollowerUserId(userId);
+		
+        return followings.stream().map(follow -> {       	
+            UserEntity followingUser = follow.getFollowing();
+            ProfileEntity followingprofile = profileRepository.findByUserEntity(followingUser);
+            
+            FollowingDTO following  = new FollowingDTO();
+            following.setFollowingUserId(followingUser.getUserId());
+            following.setFollowingLoginId(followingUser.getLoginId());
+            following.setFollowingImg(followingprofile.getProfile_img());
+            
+            return following;
+            
+        }).collect(Collectors.toList());
+	
 	}
+	/********** 유저를 팔로우하고 있는 타 유저 목록 **********/
+	public List<FollowerDTO> getFollowersByUserId(Long userId) {
+	    List<FollowEntity> followers = followRepository.findByFollowingUserId(userId);
+
+	    return followers.stream().map(follow -> {
+	        UserEntity followerUser = follow.getFollower();
+	        ProfileEntity followerProfile = profileRepository.findByUserEntity(followerUser);
+
+	        FollowerDTO follower = new FollowerDTO();
+	        follower.setFollowerUserId(followerUser.getUserId());
+	        follower.setFollowerLoginId(followerUser.getLoginId());
+	        follower.setFollowerImg(followerProfile.getProfile_img());
+
+	        return follower;
+	    }).collect(Collectors.toList());
+	}
+	
 
 }
