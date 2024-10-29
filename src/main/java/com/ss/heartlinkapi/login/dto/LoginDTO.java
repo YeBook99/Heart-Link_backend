0c8944961c24667025530eb79342203b0a96a1b0
dduddo0226 <gusbk0112@gmail.com>
2024-10-29 16:54:38 +0900

    chore: 불필요한 어노테이션 삭제

--- src/main/java/com/ss/heartlinkapi/login/dto/LoginDTO.java
+++ src/main/java/com/ss/heartlinkapi/login/dto/LoginDTO.java
@@ -1,12 +1,10 @@
 package com.ss.heartlinkapi.login.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
 public class LoginDTO {
 	
 	private String loginId;
