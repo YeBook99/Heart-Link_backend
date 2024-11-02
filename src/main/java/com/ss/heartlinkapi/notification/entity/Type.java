d570dbe7737ac92627c7b57f2441ba26e082391e
tktkaj <tktkaj@soongsil.ac.kr>
2024-11-02 17:48:19 +0900

    feat: COMMENT 변수 추가

--- src/main/java/com/ss/heartlinkapi/notification/entity/Type.java
+++ src/main/java/com/ss/heartlinkapi/notification/entity/Type.java
@@ -1,5 +1,5 @@
 package com.ss.heartlinkapi.notification.entity;
 
 public enum Type  {
-    MESSAGE_LIKE, COMMENT_LIKE, POST_LIKE, FOLLOW, PRIVATE_FOLLOW_REQUEST
+    MESSAGE_LIKE, COMMENT_LIKE, COMMENT, POST_LIKE, FOLLOW, PRIVATE_FOLLOW_REQUEST
 }
