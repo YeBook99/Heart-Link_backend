2e8e313762e911228c7dea4a26b3839a634805e0
YeBook99 <pop0131@naver.com>
2024-11-06 11:43:50 +0900

    fix 디데이 수정하는 api 수정

--- src/main/java/com/ss/heartlinkapi/couple/controller/CoupleController.java
+++ src/main/java/com/ss/heartlinkapi/couple/controller/CoupleController.java
@@ -91,8 +91,8 @@ public class CoupleController {
     }
 
     // 디데이 수정
-    @PutMapping("/dday/update/{userId}")
-    public ResponseEntity<?> updateAnniversaryDay(@AuthenticationPrincipal CustomUserDetails user, @PathVariable Long userId, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate date) {
+    @PutMapping("/dday/update")
+    public ResponseEntity<?> updateAnniversaryDay(@AuthenticationPrincipal CustomUserDetails user, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate date) {
         // 오류 500 검사
         try{
             // 오류 400 검사
@@ -100,11 +100,7 @@ public class CoupleController {
                 return ResponseEntity.badRequest().build();
             }
 
-            if (!userId.equals(user.getUserId())) {
-                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("수정 권한이 없습니다.");
-            }
-
-            CoupleEntity couple = coupleService.findByUser1_IdOrUser2_Id(userId);
+            CoupleEntity couple = coupleService.findByUser1_IdOrUser2_Id(user.getUserId());
             // 오류 404 검사
             if(couple == null) {
                 return ResponseEntity.notFound().build();
